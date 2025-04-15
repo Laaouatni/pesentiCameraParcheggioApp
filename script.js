92cf9818b69d9a1d8572763a4ddee38ce7d01f56
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-15 18:16:58 +0200

    .

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const parkingViewElement = document.getElementById("parkingViewElement");
 const parkingViewElementStyles = parkingViewElement.getBoundingClientRect();
 const parkingItems = document.querySelectorAll(".parkingItem");
 
-const FPS = 60;
+const FPS = 30;
 
 const ctx = canvasElement.getContext("2d", {
   alpha: false,
