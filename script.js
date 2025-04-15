8c795fe3e998fe81845ffaddc2c82061966ccee5
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-15 18:36:47 +0200

    save

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ const CONFIGURAZIONE = {
 
 const ctx = canvasElement.getContext("2d", {
   alpha: false,
-  // desynchronized: true,
   willReadFrequently: true,
 });
 
@@ -110,96 +109,3 @@ videoElement.addEventListener("playing", () => {
     })();
   }, 1000 / FPS);
 });
-
-
-
-// videoElement?.addEventListener("pause", () => {
-//   clearInterval(frameLogicInterval);
-// });
-
-// .then(async (stream) => {
-//   const videoTrack = stream.getVideoTracks()[0];
-//   if (!window.MediaStreamTrackProcessor) {
-//     document.body.textContent =
-//       "Aggiorna il browser o utilizza un altro browser. `window.MediaStreamTrackProcessor` non è disponibile. per dettagli vedi: https://caniuse.com/?search=mediastreamtrackprocessor";
-//     return;
-//   }
-//   const processor = new MediaStreamTrackProcessor({ track: videoTrack });
-//   const reader = processor.readable.getReader();
-
-//   canvasElement.width = parkingViewElement.clientWidth;
-//   canvasElement.height = parkingViewElement.clientHeight;
-
-//   let previousFrameArrayToSendToEsp32String = "";
-
-//   drawThisFrame();
-
-//   async function drawThisFrame() {
-//     const { value: frame, done } = await reader.read();
-//     if (done) return;
-//     ctx.drawImage(frame, 0, 0, canvasElement.width, canvasElement.height);
-//     frame.close();
-//     let thisFrameArrayToSendToEsp32 = [];
-
-//     parkingItems.forEach((thisParkingItem, thisParkingItemIndex) => {
-//       const { isOccupiedBoolean, isOccupiedConfidencePercentage } = analyzeParkingSlot(thisParkingItem);
-
-//       thisParkingItem.setAttribute("style", `--slotColor: ${isOccupiedBoolean ? "red" : "green"}`);
-//       thisParkingItem.textContent = `${isOccupiedConfidencePercentage}%`
-//       thisFrameArrayToSendToEsp32[thisParkingItemIndex] = isOccupiedBoolean ? "1" : "0";
-
-//       function analyzeParkingSlot(thisParkingItem) {
-//         let isOccupied = false;
-//         let thisColoredRatio = 0;
-//         let coloredPixelsCount = 0;
-
-//         const thisParkingItemStyles = thisParkingItem.getBoundingClientRect();
-//         const thisParkingItemPositionData = {
-//           x: thisParkingItemStyles.x - parkingViewElementStyles.x,
-//           y: thisParkingItemStyles.y - parkingViewElementStyles.y,
-//           w: thisParkingItemStyles.width,
-//           h: thisParkingItemStyles.height
-//         }
-//         const thisParkingItemData = ctx.getImageData(thisParkingItemPositionData.x, thisParkingItemPositionData.y, thisParkingItemPositionData.w, thisParkingItemPositionData.h).data;
-
-//         let totalPixels = thisParkingItemData.length / 4;
-//         for (let i = 0; i < thisParkingItemData.length; i += 4) {
-//           const thisRgbObject = {
-//             r: thisParkingItemData[i],
-//             g: thisParkingItemData[i + 1],
-//             b: thisParkingItemData[i + 2]
-//           };
-//           const thisRgbArray = [thisRgbObject.r, thisRgbObject.g, thisRgbObject.b];
-
-//           const maxDiff = Math.max(
-//             Math.abs(thisRgbArray[0] - thisRgbArray[1]),
-//             Math.abs(thisRgbArray[1] - thisRgbArray[2]),
-//             Math.abs(thisRgbArray[2] - thisRgbArray[0])
-//           );
-
-//           const isGrigio = maxDiff < CONFIGURAZIONE.tolleranzaColori.colori;
-//           const isBianco = thisRgbArray.every(thisColorValue => thisColorValue > CONFIGURAZIONE.tolleranzaColori.bianco);
-//           const isNero = thisRgbArray.every(thisColorValue => thisColorValue < CONFIGURAZIONE.tolleranzaColori.nero);
-
-//           const canIgnoreColor = isGrigio || isBianco || isNero;
-//           if (canIgnoreColor) continue;
-//           thisColoredRatio = coloredPixelsCount / totalPixels;
-//           isOccupied = thisColoredRatio > CONFIGURAZIONE.MIN_COLORED_RATIO;
-//           if (isOccupied) break;
-
-//           coloredPixelsCount++;
-//         };
-//         const isOccupiedBoolean = isOccupied;
-//         const isOccupiedConfidencePercentage = (((thisColoredRatio) / CONFIGURAZIONE.MIN_COLORED_RATIO) * 100).toFixed(2);
-//         return {
-//           isOccupiedBoolean,
-//           isOccupiedConfidencePercentage: isOccupiedConfidencePercentage > 100 ? 100 : isOccupiedConfidencePercentage,
-//         }
-//       }
-//     });
-
-//
-
-//     drawThisFrame();
-//   }
-// });
