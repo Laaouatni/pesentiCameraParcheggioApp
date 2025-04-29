2f4d8076f7e65519dc388158c41d97fa08e60cdd
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 22:55:45 +0200

    save

--- src/srcVideoLogic.js
+++ src/srcVideoLogic.js
@@ -2,12 +2,18 @@ import { FPS } from "./srcConfigCostants.js";
 import srcFrameLogic from "./frameLogic/srcFrameLogic.js";
 import { srcResizeVideoCanvas } from "./srcResizeVideoCanvas.js";
 
+export let previousFrameArrayStringToSendToEsp32 = "";
+export function updatePreviousFrameArrayStringToSendToEsp32(newValue) {
+  previousFrameArrayStringToSendToEsp32 = newValue;
+}
+
 /**
  *
  * @param {WebSocket} ws
  * @param {CanvasRenderingContext2D} ctx
  */
 function srcVideoLogic(ws, ctx) {
+  previousFrameArrayStringToSendToEsp32 = "";
   srcResizeVideoCanvas();
 
   setInterval(() => {
--- src/srcWebSocketSendingLogic.js
+++ src/srcWebSocketSendingLogic.js
@@ -1,6 +1,24 @@
+import { thisFrameArrayToSendToEsp32 } from "./frameLogic/srcFrameLogic.js";
+import {
+  previousFrameArrayStringToSendToEsp32,
+  updatePreviousFrameArrayStringToSendToEsp32,
+} from "./srcVideoLogic.js";
+
 function srcWebSocketSendingLogic(ws) {
   const isWebSocketConnected = ws.readyState === WebSocket.OPEN;
   if (!isWebSocketConnected) return;
+
+  const thisFrameArrayToSendToEsp32String =
+    thisFrameArrayToSendToEsp32.join(",");
+  const canSend =
+    thisFrameArrayToSendToEsp32String != previousFrameArrayStringToSendToEsp32;
+
+  if (!canSend) return;
+
+  ws.send(thisFrameArrayToSendToEsp32String);
+  updatePreviousFrameArrayStringToSendToEsp32(
+    thisFrameArrayToSendToEsp32String,
+  );
 }
 
 export { srcWebSocketSendingLogic };
