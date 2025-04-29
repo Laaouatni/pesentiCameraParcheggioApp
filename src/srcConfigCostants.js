d2c4fc2c16f4d29dab2300b586bac2f43daa8000
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 21:41:04 +0200

    save

--- src/srcConfigCostants.js
+++ src/srcConfigCostants.js
@@ -1,6 +1,6 @@
 export const FPS = 30;
-export const DELTA_TOLLERANZA_COLORI = 30;
 export const MIN_COLORED_RATIO = 8 / 100;
+export let DELTA_TOLLERANZA_COLORI = 30;
 export let REFERENCE_COLOR_BLACK = [0,0,0];
 export let REFERENCE_COLOR_WHITE = [255,255,255];
 
@@ -9,4 +9,7 @@ export function updateReferenceColorBlack(newValue) {
 }
 export function updateReferenceColorWhite(newValue) {
   REFERENCE_COLOR_WHITE = newValue;
+}
+export function updateDeltaTolleranzaColori(newValue) {
+  DELTA_TOLLERANZA_COLORI = newValue;
 }
--- src/srcUpdateValueConfigInputs.js
+++ src/srcUpdateValueConfigInputs.js
@@ -1,5 +1,6 @@
-import { REFERENCE_COLOR_BLACK } from "./srcConfigCostants.js";
-import { REFERENCE_COLOR_WHITE } from "./srcConfigCostants.js";
+import { REFERENCE_COLOR_BLACK, updateReferenceColorBlack } from "./srcConfigCostants.js";
+import { REFERENCE_COLOR_WHITE, updateReferenceColorWhite } from "./srcConfigCostants.js";
+import { DELTA_TOLLERANZA_COLORI, updateDeltaTolleranzaColori } from "./srcConfigCostants.js";
 
 /**
  * @type {Record<string, {input: HTMLInputElement, text: HTMLSpanElement}>}
@@ -8,10 +9,19 @@ export const domColorInputs = {
   black: {
     input: document.getElementById("blackColorInput"),
     text: document.getElementById("blackColorInputText"),
+    values: {
+      default: REFERENCE_COLOR_BLACK,
+      update: updateReferenceColorBlack,
+    }
+
   },
   white: {
     input: document.getElementById("whiteColorInput"),
     text: document.getElementById("whiteColorInputText"),
+    values: {
+      default: REFERENCE_COLOR_WHITE,
+      update: updateReferenceColorWhite,
+    }
   },
 };
 
@@ -21,19 +31,18 @@ export const domColorInputs = {
 export const inputConfigTolleranza = {
   input: document.getElementById("inputConfigTolleranza"),
   text: document.getElementById("inputConfigTolleranzaText"),
+  values: {
+    default: DELTA_TOLLERANZA_COLORI,
+    update: updateDeltaTolleranzaColori,
+  },
 };
 
 
 function srcUpdateValueConfigInputs() {
   srcUpdateColorInputsDinamically();
   srcUpdateTolleranzaInputsDinamically();
-  
-  const { input, text } = inputConfigTolleranza;
-  if (!input) throw new Error("input is not defined");
-  if (!text) throw new Error("text is not defined");
 
-  text.innerText = `${input.value}`;
-  const thisInputValue = input.value;
+
 }
 
 function srcUpdateColorInputsDinamically() {
@@ -44,20 +53,9 @@ function srcUpdateColorInputsDinamically() {
     if (!input) throw new Error("input is not defined");
     if (!text) throw new Error("text is not defined");
 
-
-    
-    // if(colorInputIndex === 0) {
-    //   input.value = `#${REFERENCE_COLOR_BLACK.toString(16).padStart(2, "0")}${REFERENCE_COLOR_BLACK.toString(16).padStart(2, "0")}${REFERENCE_COLOR_BLACK.toString(16).padStart(2, "0")}`;
-    // }
-    // if (colorInputIndex === 1) {
-    //   input.value = `#${REFERENCE_COLOR_WHITE.toString(16).padStart(2, "0")}${REFERENCE_COLOR_WHITE.toString(16).padStart(2, "0")}${REFERENCE_COLOR_WHITE.toString(16).padStart(2, "0")}`;
-    // }
-
     input.addEventListener("input", () => {
       const thisInputValue = input.value;
-      const R = parseInt(thisInputValue.slice(1, 3), 16);
-      const G = parseInt(thisInputValue.slice(3, 5), 16);
-      const B = parseInt(thisInputValue.slice(5, 7), 16);
+      const [R, G, B] = hexToRgb(thisInputValue);
       text.innerText = `rgb(${R}, ${G}, ${B})`;
     });
   });
@@ -72,6 +70,23 @@ function srcUpdateTolleranzaInputsDinamically() {
     const thisInputValue = input.value;
     text.innerText = `${thisInputValue}`;
   });
+}
+
+function rgbToHex(rgb) {
+  const hex = rgb
+    .map((color) => {
+      const hexColor = color.toString(16).padStart(2, "0");
+      return hexColor;
+    })
+    .join("");
+  return `#${hex}`;
+}
+
+function hexToRgb(hex) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return [r, g, b];
 }
 
 export { srcUpdateValueConfigInputs };
