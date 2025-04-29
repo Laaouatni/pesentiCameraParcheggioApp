f837f1d46a026281a6c6b9c381b12e774cf0b31e
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 21:05:00 +0200

    save

--- src/srcDomCostants.js
+++ src/srcDomCostants.js
@@ -1,15 +1,37 @@
 /**
  * @type {HTMLVideoElement}
-*/
+ */
 export const videoElement = document.getElementById("videoElement");
 /**
  * @type {HTMLCanvasElement}
-*/
+ */
 export const canvasElement = document.getElementById("canvasElement");
 /**
  * @type {HTMLButtonElement}
-*/
+ */
 export const btnRoutaSchermo = document.getElementById("btnRoutaSchermo");
 
 export const parkingItems = document.querySelectorAll(".parkingItem");
 export const parkingViewElement = document.getElementById("parkingViewElement");
+
+/**
+ * @type {Record<string, {input: HTMLInputElement, text: HTMLSpanElement}>}
+ */
+export const domColorInputs = {
+  black: {
+    input: document.getElementById("blackColorInput"),
+    text: document.getElementById("blackColorInputText"),
+  },
+  white: {
+    input: document.getElementById("whiteColorInput"),
+    text: document.getElementById("whiteColorInputText"),
+  },
+};
+
+/**
+ * @type {Record<string, {input: HTMLInputElement, text: HTMLSpanElement}>}
+ */
+export const inputConfigTolleranza = {
+  input: document.getElementById("inputConfigTolleranza"),
+  text: document.getElementById("inputConfigTolleranzaText"),
+};
--- src/srcUpdateValueConfigInputs.js
+++ src/srcUpdateValueConfigInputs.js
@@ -1,6 +1,24 @@
+import { domColorInputs } from "./srcDomCostants.js";
+import { inputConfigTolleranza } from "./srcDomCostants.js";
 
 function srcUpdateValueConfigInputs() {
+  const allColorInputObjects = Object.values(domColorInputs);
 
+  allColorInputObjects.forEach((colorInputObject) => {
+    const { input, text } = colorInputObject;
+    if (!input) throw new Error("input is not defined");
+    if (!text) throw new Error("text is not defined");
+
+    input.addEventListener("input", () => {
+      const thisInputValue = input.value;
+      const R = parseInt(thisInputValue.slice(1, 3), 16);
+      const G = parseInt(thisInputValue.slice(3, 5), 16);
+      const B = parseInt(thisInputValue.slice(5, 7), 16);
+      text.innerText = `rgb(${R}, ${G}, ${B})`;
+    });
+  });
+
+  
 }
 
 export { srcUpdateValueConfigInputs };
