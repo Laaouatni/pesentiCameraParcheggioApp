ce24ca7a08699c08c3fdcdd0c1d774e1a5d46f6f
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 20:43:35 +0200

    save

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 import srcVideoLogic from "./src/srcVideoLogic.js";
+import { srcCreateCtx } from "./src/srcCreateCtx.js";
 import { videoElement } from "./src/srcDomCostants.js";
 import { srcFullScreenBtn } from "./src/srcFullScreenBtn.js";
 import { srcActivateVideoCamera } from "./src/srcActivateCamera.js";
 import { srcDontTurnOffTheSite } from "./src/srcDontTurnOffTheSite.js";
 import { srcCreateWebSocketClient } from "./src/srcCreateWebSocketClient.js";
-import { srcCreateCtx } from "./src/srcCreateCtx.js";
+import { srcUpdateValueConfigInputs } from "./src/srcUpdateValueConfigInputs.js";
 
 const ws = srcCreateWebSocketClient();
 const ctx = srcCreateCtx();
@@ -12,6 +13,7 @@ const ctx = srcCreateCtx();
 srcFullScreenBtn();
 srcDontTurnOffTheSite();
 srcActivateVideoCamera();
+srcUpdateValueConfigInputs();
 
 videoElement.addEventListener("playing", () => {
   if (!ctx) throw new Error("ctx is not defined");
---
+++ src/srcUpdateValueConfigInputs.js
@@ -0,0 +1,5 @@
+function srcUpdateValueConfigInputs() {
+
+}
+
+export { srcUpdateValueConfigInputs };
