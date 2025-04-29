c5f5fe0691f1037f8f927c0e57b8d20fa8ee30b5
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 17:31:35 +0200

    save

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+import srcVideoLogic from "./src/srcVideoLogic.js";
+import { videoElement } from "./src/srcDomCostants.js";
+import { srcFullScreenBtn } from "./src/srcFullScreenBtn.js";
 import { srcActivateVideoCamera } from "./src/srcActivateCamera.js";
 import { srcDontTurnOffTheSite } from "./src/srcDontTurnOffTheSite.js";
-import { srcFullScreenBtn } from "./src/srcFullScreenBtn.js";
-import { videoElement } from "./src/srcDomCostants.js";
-import srcVideoLogic from "./src/srcVideoLogic.js";
 
 const ws = new WebSocket("wss://pesentiws-43f6274c0f11.herokuapp.com/");
 
@@ -20,18 +20,4 @@ videoElement.addEventListener("playing", () => {
 
 // const parkingViewElementStyles = parkingViewElement.getBoundingClientRect();
 // const parkingItems = document.querySelectorAll(".parkingItem");
-// const CONFIGURAZIONE = {
-//   tolleranzaColori: {
-//     GENERALE: 30,
-//     get colori() {
-//       return this.GENERALE;
-//     },
-//     get bianco() {
-//       return 255 - this.GENERALE;
-//     },
-//     get nero() {
-//       return this.GENERALE;
-//     },
-//   },
-//   MIN_COLORED_RATIO: 8 / 100,
-// };
+
--- src/srcConfigCostants.js
+++ src/srcConfigCostants.js
@@ -1,1 +1,19 @@
 export const FPS = 30;
+export const DELTA_TOLLERANZA_COLORI = 30;
+export const MIN_COLORED_RATIO = 8 / 100;
+
+// const CONFIGURAZIONE = {
+//   tolleranzaColori: {
+//     GENERALE: 30,
+//     get colori() {
+//       return this.GENERALE;
+//     },
+//     get bianco() {
+//       return 255 - this.GENERALE;
+//     },
+//     get nero() {
+//       return this.GENERALE;
+//     },
+//   },
+//   MIN_COLORED_RATIO: 8 / 100,
+// };
