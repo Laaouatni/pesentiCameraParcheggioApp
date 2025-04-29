7abf1c76531ce83f54c68ee205036cabce329f1f
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 17:34:09 +0200

    save

--- src/srcConfigCostants.js
+++ src/srcConfigCostants.js
@@ -1,19 +1,5 @@
 export const FPS = 30;
 export const DELTA_TOLLERANZA_COLORI = 30;
 export const MIN_COLORED_RATIO = 8 / 100;
-
-// const CONFIGURAZIONE = {
-//   tolleranzaColori: {
-//     GENERALE: 30,
-//     get colori() {
-//       return this.GENERALE;
-//     },
-//     get bianco() {
-//       return 255 - this.GENERALE;
-//     },
-//     get nero() {
-//       return this.GENERALE;
-//     },
-//   },
-//   MIN_COLORED_RATIO: 8 / 100,
-// };
+export let REFERENCE_COLOR_BLACK = DELTA_TOLLERANZA_COLORI;
+export let REFERENCE_COLOR_WHITE = 255 - DELTA_TOLLERANZA_COLORI;
