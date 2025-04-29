1e709b81f9aa023c02136883fed454c9880a0226
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 18:20:37 +0200

    save

--- src/srcCreateWebSocketClient.js
+++ src/srcCreateWebSocketClient.js
@@ -1,5 +1,6 @@
 function srcCreateWebSocketClient() {
-  const ws = new WebSocket("wss://pesentiws-43f6274c0f11.herokuapp.com/");
+  const URL = "pesentiws-43f6274c0f11.herokuapp.com/";
+  const ws = new WebSocket(`wss://${URL}`);
 
   ws.addEventListener("close", () => {
     window.location.reload();
