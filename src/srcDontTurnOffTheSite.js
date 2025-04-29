cb20c55e7161c639457ecd580c15c89f3e6f4681
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 20:47:32 +0200

    save

--- src/srcDontTurnOffTheSite.js
+++ src/srcDontTurnOffTheSite.js
@@ -2,8 +2,7 @@ function srcDontTurnOffTheSite() {
   if (!("wakeLock" in navigator)) return;
 
   navigator.wakeLock.request("screen").catch((err) => {
-    console.error(`Wake Lock request failed: ${err.name}, ${err.message}`);
-    window.location.reload();
+    alert(`refresha la pagina: Wake Lock request failed: ${err.name}, ${err.message}`);
   });
 }
 
