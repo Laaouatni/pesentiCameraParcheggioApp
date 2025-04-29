f41dd0660af560078ad145f744fe241047a08cb9
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 19:14:20 +0200

    save

--- src/frameLogic/srcAnalyzeParkingSlot.js
+++ src/frameLogic/srcAnalyzeParkingSlot.js
@@ -1,4 +1,5 @@
-import { srcGetThisParkingItemPositionData } from "./srcGetThisParkingItemPositionData";
+import { DELTA_TOLLERANZA_COLORI, MIN_COLORED_RATIO, REFERENCE_COLOR_BLACK, REFERENCE_COLOR_WHITE } from "../srcConfigCostants.js";
+import { srcGetThisParkingItemPositionData } from "./srcGetThisParkingItemPositionData.js";
 
 /**
  *
@@ -26,42 +27,35 @@ function srcAnalyzeParkingSlot(ctx, thisParkingItem, parkingViewElementStyles) {
     thisParkingItemPositionData.h,
   ).data;
 
-  return false;
-  //         for (let i = 0; i < thisParkingItemData.length; i += 4) {
-  //           const thisRgbObject = {
-  //             r: thisParkingItemData[i],
-  //             g: thisParkingItemData[i + 1],
-  //             b: thisParkingItemData[i + 2],
-  //           };
-  //           const thisRgbArray = [
-  //             thisRgbObject.r,
-  //             thisRgbObject.g,
-  //             thisRgbObject.b,
-  //           ];
-  //           const maxDiff = Math.max(
-  //             Math.abs(thisRgbArray[0] - thisRgbArray[1]),
-  //             Math.abs(thisRgbArray[1] - thisRgbArray[2]),
-  //             Math.abs(thisRgbArray[2] - thisRgbArray[0]),
-  //           );
-  //           const isGrigio = maxDiff < CONFIGURAZIONE.tolleranzaColori.colori;
-  //           const isBianco = thisRgbArray.every(
-  //             (thisColorValue) =>
-  //               thisColorValue > CONFIGURAZIONE.tolleranzaColori.bianco,
-  //           );
-  //           const isNero = thisRgbArray.every(
-  //             (thisColorValue) =>
-  //               thisColorValue < CONFIGURAZIONE.tolleranzaColori.nero,
-  //           );
-  //           const canIgnoreColor = isGrigio || isBianco || isNero;
-  //           if (canIgnoreColor) continue;
-  //           const totalPixels = thisParkingItemData.length / 4;
-  //           thisColoredRatio = coloredPixelsCount / totalPixels;
-  //           isOccupied = thisColoredRatio > CONFIGURAZIONE.MIN_COLORED_RATIO;
-  //           if (isOccupied) break;
-  //           coloredPixelsCount++;
-  //         }
-  //         return isOccupied;
-  //       }
+  for (let i = 0; i < thisParkingItemData.length; i += 4) {
+    const thisRgbObject = {
+      r: thisParkingItemData[i],
+      g: thisParkingItemData[i + 1],
+      b: thisParkingItemData[i + 2],
+    };
+    const thisRgbArray = [thisRgbObject.r, thisRgbObject.g, thisRgbObject.b];
+    const maxDiff = Math.max(
+      Math.abs(thisRgbArray[0] - thisRgbArray[1]),
+      Math.abs(thisRgbArray[1] - thisRgbArray[2]),
+      Math.abs(thisRgbArray[2] - thisRgbArray[0]),
+    );
+    const isGrigio = maxDiff < DELTA_TOLLERANZA_COLORI;
+    const isBianco = thisRgbArray.every(
+      (thisColorValue) =>
+        thisColorValue > REFERENCE_COLOR_WHITE,
+    );
+    const isNero = thisRgbArray.every(
+      (thisColorValue) => thisColorValue < REFERENCE_COLOR_BLACK,
+    );
+    const canIgnoreColor = isGrigio || isBianco || isNero;
+    if (canIgnoreColor) continue;
+    const totalPixels = thisParkingItemData.length / 4;
+    thisColoredRatio = coloredPixelsCount / totalPixels;
+    isOccupied = thisColoredRatio > MIN_COLORED_RATIO;
+    if (isOccupied) break;
+    coloredPixelsCount++;
+  }
+  return isOccupied;
 }
 
 export { srcAnalyzeParkingSlot };
