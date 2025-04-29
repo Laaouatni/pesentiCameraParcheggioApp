b8a0569ba468935777e5aaae8a8ee60479b64861
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-29 16:56:11 +0200

    save

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { srcDontTurnOffTheSite } from "./src/srcDontTurnOffTheSite.js";
 import { srcFullScreenBtn } from "./src/srcFullScreenBtn.js";
 
+const FPS = 30;
 const ws = new WebSocket("wss://pesentiws-43f6274c0f11.herokuapp.com/");
 
 ws.addEventListener("close", () => {
@@ -11,145 +12,145 @@ srcFullScreenBtn();
 srcDontTurnOffTheSite();
 
 
-const canvasElement = document.getElementById("canvasElement");
-const videoElement = document.getElementById("videoElement");
 
-const parkingViewElement = document.getElementById("parkingViewElement");
-const parkingViewElementStyles = parkingViewElement.getBoundingClientRect();
-const parkingItems = document.querySelectorAll(".parkingItem");
+// const canvasElement = document.getElementById("canvasElement");
+// const videoElement = document.getElementById("videoElement");
 
-const FPS = 30;
+// const parkingViewElement = document.getElementById("parkingViewElement");
+// const parkingViewElementStyles = parkingViewElement.getBoundingClientRect();
+// const parkingItems = document.querySelectorAll(".parkingItem");
 
-const CONFIGURAZIONE = {
-  tolleranzaColori: {
-    GENERALE: 30,
-    get colori() {
-      return this.GENERALE;
-    },
-    get bianco() {
-      return 255 - this.GENERALE;
-    },
-    get nero() {
-      return this.GENERALE;
-    },
-  },
-  MIN_COLORED_RATIO: 8 / 100,
-};
 
-const ctx = canvasElement.getContext("2d", {
-  alpha: false,
-  willReadFrequently: true,
-});
+// const CONFIGURAZIONE = {
+//   tolleranzaColori: {
+//     GENERALE: 30,
+//     get colori() {
+//       return this.GENERALE;
+//     },
+//     get bianco() {
+//       return 255 - this.GENERALE;
+//     },
+//     get nero() {
+//       return this.GENERALE;
+//     },
+//   },
+//   MIN_COLORED_RATIO: 8 / 100,
+// };
 
-navigator.mediaDevices
-  .getUserMedia({
-    video: {
-      facingMode: "environment",
-      aspectRatio:
-        parkingViewElement.clientWidth / parkingViewElement.clientHeight,
-    },
-  })
-  .then(async (stream) => {
-    videoElement.srcObject = stream;
-  });
+// const ctx = canvasElement.getContext("2d", {
+//   alpha: false,
+//   willReadFrequently: true,
+// });
 
-videoElement.addEventListener("playing", () => {
-  canvasElement.width = parkingViewElement.clientWidth;
-  canvasElement.height = parkingViewElement.clientHeight;
+// navigator.mediaDevices
+//   .getUserMedia({
+//     video: {
+//       facingMode: "environment",
+//       aspectRatio:
+//         parkingViewElement.clientWidth / parkingViewElement.clientHeight,
+//     },
+//   })
+//   .then(async (stream) => {
+//     videoElement.srcObject = stream;
+//   });
 
-  let previousFrameArrayToSendToEsp32String = "";
+// videoElement.addEventListener("playing", () => {
+//   canvasElement.width = parkingViewElement.clientWidth;
+//   canvasElement.height = parkingViewElement.clientHeight;
 
-  setInterval(() => {
-    ctx.drawImage(
-      videoElement,
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height,
-    );
-    let thisFrameArrayToSendToEsp32 = [];
+//   let previousFrameArrayToSendToEsp32String = "";
 
-    parkingItems.forEach((thisParkingItem, thisParkingItemIndex) => {
-      const isOccupiedBoolean = analyzeParkingSlot(thisParkingItem);
-      thisParkingItem.setAttribute(
-        "style",
-        `--slotColor: ${isOccupiedBoolean ? "red" : "green"}`,
-      );
-      thisFrameArrayToSendToEsp32[thisParkingItemIndex] = isOccupiedBoolean
-        ? "1"
-        : "0";
+//   setInterval(() => {
+//     ctx.drawImage(
+//       videoElement,
+//       0,
+//       0,
+//       canvasElement.width,
+//       canvasElement.height,
+//     );
+//     let thisFrameArrayToSendToEsp32 = [];
 
-      function analyzeParkingSlot(thisParkingItem) {
-        let isOccupied = false;
-        let thisColoredRatio = 0;
-        let coloredPixelsCount = 0;
+//     parkingItems.forEach((thisParkingItem, thisParkingItemIndex) => {
+//       const isOccupiedBoolean = analyzeParkingSlot(thisParkingItem);
+//       thisParkingItem.setAttribute(
+//         "style",
+//         `--slotColor: ${isOccupiedBoolean ? "red" : "green"}`,
+//       );
+//       thisFrameArrayToSendToEsp32[thisParkingItemIndex] = isOccupiedBoolean
+//         ? "1"
+//         : "0";
 
-        const thisParkingItemStyles = thisParkingItem.getBoundingClientRect();
-        const thisParkingItemPositionData = {
-          x: thisParkingItemStyles.x - parkingViewElementStyles.x,
-          y: thisParkingItemStyles.y - parkingViewElementStyles.y,
-          w: thisParkingItemStyles.width,
-          h: thisParkingItemStyles.height,
-        };
-        const thisParkingItemData = ctx.getImageData(
-          thisParkingItemPositionData.x,
-          thisParkingItemPositionData.y,
-          thisParkingItemPositionData.w,
-          thisParkingItemPositionData.h,
-        ).data;
+//       function analyzeParkingSlot(thisParkingItem) {
+//         let isOccupied = false;
+//         let thisColoredRatio = 0;
+//         let coloredPixelsCount = 0;
 
-        let totalPixels = thisParkingItemData.length / 4;
-        for (let i = 0; i < thisParkingItemData.length; i += 4) {
-          const thisRgbObject = {
-            r: thisParkingItemData[i],
-            g: thisParkingItemData[i + 1],
-            b: thisParkingItemData[i + 2],
-          };
-          const thisRgbArray = [
-            thisRgbObject.r,
-            thisRgbObject.g,
-            thisRgbObject.b,
-          ];
+//         const thisParkingItemStyles = thisParkingItem.getBoundingClientRect();
+//         const thisParkingItemPositionData = {
+//           x: thisParkingItemStyles.x - parkingViewElementStyles.x,
+//           y: thisParkingItemStyles.y - parkingViewElementStyles.y,
+//           w: thisParkingItemStyles.width,
+//           h: thisParkingItemStyles.height,
+//         };
+//         const thisParkingItemData = ctx.getImageData(
+//           thisParkingItemPositionData.x,
+//           thisParkingItemPositionData.y,
+//           thisParkingItemPositionData.w,
+//           thisParkingItemPositionData.h,
+//         ).data;
 
-          const maxDiff = Math.max(
-            Math.abs(thisRgbArray[0] - thisRgbArray[1]),
-            Math.abs(thisRgbArray[1] - thisRgbArray[2]),
-            Math.abs(thisRgbArray[2] - thisRgbArray[0]),
-          );
+//         let totalPixels = thisParkingItemData.length / 4;
+//         for (let i = 0; i < thisParkingItemData.length; i += 4) {
+//           const thisRgbObject = {
+//             r: thisParkingItemData[i],
+//             g: thisParkingItemData[i + 1],
+//             b: thisParkingItemData[i + 2],
+//           };
+//           const thisRgbArray = [
+//             thisRgbObject.r,
+//             thisRgbObject.g,
+//             thisRgbObject.b,
+//           ];
 
-          const isGrigio = maxDiff < CONFIGURAZIONE.tolleranzaColori.colori;
-          const isBianco = thisRgbArray.every(
-            (thisColorValue) =>
-              thisColorValue > CONFIGURAZIONE.tolleranzaColori.bianco,
-          );
-          const isNero = thisRgbArray.every(
-            (thisColorValue) =>
-              thisColorValue < CONFIGURAZIONE.tolleranzaColori.nero,
-          );
+//           const maxDiff = Math.max(
+//             Math.abs(thisRgbArray[0] - thisRgbArray[1]),
+//             Math.abs(thisRgbArray[1] - thisRgbArray[2]),
+//             Math.abs(thisRgbArray[2] - thisRgbArray[0]),
+//           );
 
-          const canIgnoreColor = isGrigio || isBianco || isNero;
-          if (canIgnoreColor) continue;
-          thisColoredRatio = coloredPixelsCount / totalPixels;
-          isOccupied = thisColoredRatio > CONFIGURAZIONE.MIN_COLORED_RATIO;
-          if (isOccupied) break;
+//           const isGrigio = maxDiff < CONFIGURAZIONE.tolleranzaColori.colori;
+//           const isBianco = thisRgbArray.every(
+//             (thisColorValue) =>
+//               thisColorValue > CONFIGURAZIONE.tolleranzaColori.bianco,
+//           );
+//           const isNero = thisRgbArray.every(
+//             (thisColorValue) =>
+//               thisColorValue < CONFIGURAZIONE.tolleranzaColori.nero,
+//           );
 
-          coloredPixelsCount++;
-        }
-        return isOccupied;
-      }
-    });
+//           const canIgnoreColor = isGrigio || isBianco || isNero;
+//           if (canIgnoreColor) continue;
+//           thisColoredRatio = coloredPixelsCount / totalPixels;
+//           isOccupied = thisColoredRatio > CONFIGURAZIONE.MIN_COLORED_RATIO;
+//           if (isOccupied) break;
 
-    const isWSconnected = ws.readyState === WebSocket.OPEN;
-    (() => {
-      if (!isWSconnected) return;
-      const thisFrameArrayToSendToEsp32String =
-        thisFrameArrayToSendToEsp32.join(",");
-      const canSend =
-        thisFrameArrayToSendToEsp32String !=
-        previousFrameArrayToSendToEsp32String;
-      if (!canSend) return;
-      ws.send(thisFrameArrayToSendToEsp32String);
-      previousFrameArrayToSendToEsp32String = thisFrameArrayToSendToEsp32String;
-    })();
-  }, 1000 / FPS);
-});
+//           coloredPixelsCount++;
+//         }
+//         return isOccupied;
+//       }
+//     });
+
+//     const isWSconnected = ws.readyState === WebSocket.OPEN;
+//     (() => {
+//       if (!isWSconnected) return;
+//       const thisFrameArrayToSendToEsp32String =
+//         thisFrameArrayToSendToEsp32.join(",");
+//       const canSend =
+//         thisFrameArrayToSendToEsp32String !=
+//         previousFrameArrayToSendToEsp32String;
+//       if (!canSend) return;
+//       ws.send(thisFrameArrayToSendToEsp32String);
+//       previousFrameArrayToSendToEsp32String = thisFrameArrayToSendToEsp32String;
+//     })();
+//   }, 1000 / FPS);
+// });
