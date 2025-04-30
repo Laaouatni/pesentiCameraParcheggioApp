ead7ad74442bc481f24a35b398295cecfbf7ad86
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2025-04-30 08:13:19 +0200

    Update srcDomCostants.js

--- src/srcDomCostants.js
+++ src/srcDomCostants.js
@@ -13,25 +13,3 @@ export const btnRoutaSchermo = document.getElementById("btnRoutaSchermo");
 
 export const parkingItems = document.querySelectorAll(".parkingItem");
 export const parkingViewElement = document.getElementById("parkingViewElement");
-
-/**
- * @type {Record<string, {input: HTMLInputElement, text: HTMLSpanElement}>}
- */
-export const domColorInputs = {
-  black: {
-    input: document.getElementById("blackColorInput"),
-    text: document.getElementById("blackColorInputText"),
-  },
-  white: {
-    input: document.getElementById("whiteColorInput"),
-    text: document.getElementById("whiteColorInputText"),
-  },
-};
-
-/**
- * @type {{input: HTMLInputElement, text: HTMLSpanElement}}
- */
-export const inputConfigTolleranza = {
-  input: document.getElementById("inputConfigTolleranza"),
-  text: document.getElementById("inputConfigTolleranzaText"),
-};
